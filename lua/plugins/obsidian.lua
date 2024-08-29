2936a44da43e726a92395f1b70a683468051a9d1
Chuk <chuk@loki.local>
2024-08-29 17:28:05 +0200

    obsidian front matter bug

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -39,20 +39,45 @@ return {
       date_format = "%Y-%m-%d-%a",
       time_format = "%H:%M",
     },
-
+    daily_notes = {
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes", "tasks" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "daily-note.md",
+    },
     note_frontmatter_func = function(note)
       -- This is equivalent to the default frontmatter function.
       local out = { id = note.id, aliases = note.aliases, tags = note.tags }
       -- `note.metadata` contains any manually added fields in the frontmatter.
       -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
         for k, v in pairs(note.metadata) do
           out[k] = v
         end
       end
       return out
     end,
-
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      local timestamp = os.time()
+      local formated_timestamp = os.date("%Y-%m-%dT%H_%M_%S", timestamp)
+      return formated_timestamp .. "-" .. suffix
+    end,
     -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
     -- URL it will be ignored but you can customize this behavior here.
     follow_url_func = vim.ui.open or function(url) require("astrocore").system_open(url) end,
